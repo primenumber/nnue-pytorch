4bab2ce6aff26a93f482bfb975293fc63f245de5
primenumber <prime@kmc.gr.jp>
2025-06-08 02:36:19 +0900

    Use tensor core for matmul

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from torch.utils.data import DataLoader, Dataset
 
 
 def cli_main():
+  torch.set_float32_matmul_precision('high')
   cli = LightningCLI(M.NNUE, nnue_data_module.NNUEDataModule)
 
 if __name__ == '__main__':
