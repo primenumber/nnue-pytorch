e68ee3501a692ba3c09632d5cae622c715ad1ec2
primenumber <prime@kmc.gr.jp>
2026-06-27 16:45:55 +0900

    Use HalfKAv2_hm by default

--- serialize.sh
+++ serialize.sh
@@ -4,4 +4,4 @@ set -eux
 # Pick the most recently modified checkpoint under lightning_logs
 ckpt=$(find lightning_logs -name '*.ckpt' -printf '%T@ %p\n' | sort -n | tail -1 | cut -d' ' -f2-)
 
-uv run serialize.py --features="HalfKAHm" "$ckpt" eval_issen/nn.nnue
+uv run serialize.py --features="HalfKAv2_hm" "$ckpt" eval_issen/nn.nnue
